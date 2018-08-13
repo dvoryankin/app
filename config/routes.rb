9422178d67b91fb95df972286175ffe9e76118ca
Dmitry <pchellovod@gmail.com>
2018-08-13 23:24:13 +0500

    fix help_path in _header partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get  '/help',    to: 'static_pages#help', as: :help_path
+  get  '/help',    to: 'static_pages#help', as: :help
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
 
